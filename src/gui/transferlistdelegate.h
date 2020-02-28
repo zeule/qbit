ndduc/qBittorrent

Use QStyledItemDelegate instead of QItemDelegate

src/gui/transferlistdelegate.h
@@ -26,24 +26,23 @@
  * exception statement from your version.
  */
 
-#ifndef TRANSFERLISTDELEGATE_H
-#define TRANSFERLISTDELEGATE_H
+#pragma once
 
-#include <QItemDelegate>
+#include <QStyledItemDelegate>
 
 class QModelIndex;
 class QPainter;
 class QStyleOptionViewItem;
 
-class TransferListDelegate : public QItemDelegate
+class TransferListDelegate : public QStyledItemDelegate
 {
     Q_OBJECT
+    Q_DISABLE_COPY_MOVE(TransferListDelegate)
 
 public:
-    TransferListDelegate(QObject *parent);
+    explicit TransferListDelegate(QObject *parent);
+
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
     QWidget *createEditor(QWidget *, const QStyleOptionViewItem &, const QModelIndex &) const override;
     QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 };
-
-#endif // TRANSFERLISTDELEGATE_H

zeule/qbit

src/gui/transferlistdelegate.h
@@ -26,24 +26,23 @@
  * exception statement from your version.
  */
 
-#ifndef TRANSFERLISTDELEGATE_H
-#define TRANSFERLISTDELEGATE_H
+#pragma once
 
-#include <QItemDelegate>
+#include <QStyledItemDelegate>
 
 class QModelIndex;
 class QPainter;
 class QStyleOptionViewItem;
 
-class TransferListDelegate : public QItemDelegate
+class TransferListDelegate : public QStyledItemDelegate
 {
     Q_OBJECT
+    Q_DISABLE_COPY(TransferListDelegate)
 
 public:
-    TransferListDelegate(QObject *parent);
+    explicit TransferListDelegate(QObject *parent);
+
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
     QWidget *createEditor(QWidget *, const QStyleOptionViewItem &, const QModelIndex &) const override;
     QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 };
-
-#endif // TRANSFERLISTDELEGATE_H